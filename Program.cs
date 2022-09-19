6e25ce54e66d0243a358340d7ed647a64e47a730
Pavel Anokhin <anohinpa@gmail.com>
2022-09-19 21:39:24 +0300

    Task4

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿// Task 2. Compare 2 numbers
+﻿// Task 4. Compare 2 numbers
 Console.Write("Введите первое число: \n");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: \n");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: \n");
+int c = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
-    Console.Write($"Первое число {a} больше второго {b}");
-}
-else if (a < b)
-{
-    Console.Write($"Второе число {b} больше первого {a}");
+    if (a > c)    {        Console.Write($"Максимальное число: {a}");    }
+    else    {        Console.Write($"Максимальное число: {c}");    }
 }
 else
 {
-    Console.Write("Введенные числа равны");
+    if (b > c)    {        Console.Write($"Максимальное число: {b}");    }
+    else    {        Console.Write($"Максимальное число: {c}");    }
 }
